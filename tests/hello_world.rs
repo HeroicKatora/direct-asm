d30e777e3e26868d79de6b2d5d0eb4e1bdc96a83
Andreas Molzer <andreas.molzer@gmx.de>
2019-11-04 01:21:22 +0100

    Fix mismatch with variadics
    The libc::printf function is a variadic one thus it is unsafe to pretend
    it to be a non-variadic one. Corrects this by providing another wrapper
    around with the correct call ABI.

--- tests/hello_world.rs
+++ tests/hello_world.rs
@@ -1,12 +1,16 @@
 #[direct_asm::assemble]
-unsafe fn call_this(rdi: *const u8, rsi: unsafe extern "C" fn(*const i8, ...) -> libc::c_int) {
+unsafe fn call_this(rdi: *const u8, rsi: unsafe extern "C" fn(*const i8)) {
     "call rsi
 ret"
+}
+
+unsafe extern "C" fn printf(what: *const i8) {
+    libc::printf(what);
 }
 
 static HELLO: &[u8] = b"Hello, world!\0";
 
 #[test]
 fn run_hello() {
-    unsafe { call_this(HELLO.as_ptr(), libc::printf) }
+    unsafe { call_this(HELLO.as_ptr(), printf) }
 }
