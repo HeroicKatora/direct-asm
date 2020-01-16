fbc508dcb1aadd2b333fe57b65e68443ad6be44f
HeroicKatora <andreas.molzer@gmx.de>
2020-01-16 20:15:30 +0100

    Add failing test: Include outer constant in code

--- tests/system_call.rs
+++ tests/system_call.rs
@@ -2,10 +2,10 @@ use libc::{c_int, c_void, size_t, ssize_t, c_long, SYS_write};
 
 #[direct_asm::assemble]
 unsafe extern "C" 
-fn sys_write(fd: c_int, ptr: *const c_void, len: size_t, wcall: c_long)
+fn sys_write(fd: c_int, ptr: *const c_void, len: size_t)
     -> ssize_t 
 {
-    "mov %rax, %rcx"; // Move sys call number to rax as required
+    "mov %rax, ${LONG SYS_write}"; // Move sys call number to rax as required
     // Other arguments are already in correct register
     "syscall"; // Invoke actual system call placed in rax
     "ret"; //Return actual result
@@ -13,7 +13,7 @@ fn sys_write(fd: c_int, ptr: *const c_void, len: size_t, wcall: c_long)
 
 fn sys_print(what: &str) -> libc::ssize_t {
     unsafe {
-        sys_write(1, what.as_ptr() as *const libc::c_void, what.len(), SYS_write)
+        sys_write(1, what.as_ptr() as *const libc::c_void, what.len())
     }
 }
 
